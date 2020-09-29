09d4f03e565ac9780dc04e6f73641c74386219ac
Daniil Trofimov <trofimovdanil946@gmail.com>
2020-09-29 22:52:32 +0300

    Added test servlet & registration layout

---
+++ homeworks/homework #5 (Registration)/src/main/java/Registration/SimpleServlet.java
@@ -0,0 +1,55 @@
+package Registration;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * This is one of the simplest example.
+ */
+public class SimpleServlet extends HttpServlet{
+  
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    PrintWriter out = resp.getWriter();
+    out.println(getPageCode(""));
+  }
+  
+  @Override
+  protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    String name = req.getParameter("name");
+    if(name.isEmpty()){
+      name = "Stranger";
+    }
+    String greetings = "<h1>Hello "+name+"!</h1>";
+    
+    PrintWriter out = resp.getWriter();
+    out.println(getPageCode(greetings));
+  }
+  
+  /**
+   * Helper that generates page code for both GET and POST.
+   * @param content Data to output after showing form.
+   * @return HTML content for page.
+   */
+  protected String getPageCode(String content){
+    return "<!DOCTYPE html><html>"
+            + "<head><meta charset='UTF-8'><title>Hello page</title></head>"
+            + "<body style='height: 100vh; display: grid; place-items: center'>"
+            + "<form action='' method='POST' style='display: flex; flex-direction: column; gap: 10px; width: 400px'>" +
+            "<h4>Registration</h4>"+
+            "<input type='text' placeholder='Name' name='name'>" +
+            "<input type='text' placeholder='Soname' name='soname'>"+
+            "<input type='password' placeholder='Password' name='password'>"+
+            "<input type='password' placeholder='Repeat password' name='password-check'>"+
+            "Terms and conditions <br>"+
+            "<input type='checkbox' name='rules'>"+
+            "<input type='submit' value='register'>"+
+            "</form>"
+            + content
+            + "</body></html>";
+  }
+}
