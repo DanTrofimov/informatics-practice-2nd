3189ed72ca0d44bd71bdd27e314028a2d3cfe0f2
Daniil Trofimov <trofimovdanil946@gmail.com>
2020-11-06 18:36:42 +0300

    fine work #2

---
+++ homeworks/REST interface/src/main/java/REST/servlets/RestServlet.java
@@ -0,0 +1,18 @@
+package REST.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/")
+public class RestServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, java.io.IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, java.io.IOException {
+        getServletContext().getRequestDispatcher("/main.jsp").forward(request, response);
+    }
+}
---
+++ homeworks/REST interface/target/REST-1.0-SNAPSHOT/main.jsp
@@ -0,0 +1,11 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+
+<html>
+<head>
+    <title>REST</title>
+</head>
+    <body>
+        <h1>rest service</h1>
+    </body>
+</html>
