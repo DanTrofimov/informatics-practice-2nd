6a54565b93fd30a9ba4df1e083400316e86866d6
Daniil Trofimov <trofimovdanil946@gmail.com>
2021-03-07 00:46:20 +0300

    Update PageRepository.java

--- homeworks sem 2/homework #6 (CMS)/src/main/java/ru/itis/trofimoff/repositories/PageRepository.java
+++ homeworks sem 2/homework #6 (CMS)/src/main/java/ru/itis/trofimoff/repositories/PageRepository.java
@@ -3,7 +3,5 @@ package ru.itis.trofimoff.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.itis.trofimoff.models.Page;
 
-import java.util.Optional;
-
 public interface PageRepository extends JpaRepository<Page, Long>{
 }
