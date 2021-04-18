e17774f47bb431b81856ac019da3f34d3bf11702
Daniil Trofimov <trofimovdanil946@gmail.com>
2021-04-18 13:04:28 +0300

    init

---
+++ homeworks sem 2/homework #16 (AJAX + jQuery)/src/main/java/ru/itis/trofimoff/ajax/config/AppConfig.java
@@ -0,0 +1,20 @@
+package ru.itis.trofimoff.ajax.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerViewResolver;
+
+@Configuration
+public class AppConfig implements WebMvcConfigurer {
+    // view resolver
+    @Bean
+    public FreeMarkerViewResolver freemarkerViewResolver() {
+        FreeMarkerViewResolver resolver = new FreeMarkerViewResolver();
+        resolver.setPrefix("");
+        resolver.setSuffix(".ftl");
+        resolver.setContentType("text/html;charset=UTF-8");
+
+        return resolver;
+    }
+}
---
+++ homeworks sem 2/homework #16 (AJAX + jQuery)/src/main/java/ru/itis/trofimoff/ajax/controllers/DefaultContoller.java
@@ -0,0 +1,14 @@
+package ru.itis.trofimoff.ajax.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DefaultContoller {
+
+    @GetMapping("/table")
+    public String getTable() {
+        return "table";
+    }
+}
