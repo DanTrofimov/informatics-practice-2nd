244b5a7a0a00b6669bd5ded4d222f35c8947a603
Daniil Trofimov <trofimovdanil946@gmail.com>
2021-04-18 12:44:56 +0300

    project init

---
+++ homeworks sem 2/homework #16 (AJAX + jQuery)/src/test/java/ru/itis/trofimoff/ajax/AjaxApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.itis.trofimoff.ajax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AjaxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
