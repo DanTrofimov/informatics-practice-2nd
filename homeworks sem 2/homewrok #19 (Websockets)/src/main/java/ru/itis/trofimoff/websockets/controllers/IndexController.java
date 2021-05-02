74904c6f25d5d961f972bbe9370f166f51e38477
Daniil Trofimov <trofimovdanil946@gmail.com>
2021-05-02 18:41:22 +0300

    websockets

---
+++ homeworks sem 2/homewrok #19 (Websockets)/src/main/java/ru/itis/trofimoff/websockets/controllers/IndexController.java
@@ -0,0 +1,15 @@
+package ru.itis.trofimoff.websockets.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import java.util.UUID;
+
+@Controller
+public class IndexController {
+    @GetMapping("/index")
+    public String getIndexPage(Model model) {
+        model.addAttribute("pageId", UUID.randomUUID().toString());
+        return "index";
+    }
+}
